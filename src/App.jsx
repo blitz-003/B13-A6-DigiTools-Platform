bdcbe46051020287ec2e77447ceae5c122d5e865
Elman Pathan <18302016003@fudan.edu.cn>
2026-03-31 23:25:46 +0600

    2nd commit. stat section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
-import Hero from "./components/Navbar/Hero/Hero";
+import Hero from "./components/Hero/Hero";
+import Statistics from "./components/Statistics/Statistics";
 
 function App() {
   return (
     <>
       <Navbar></Navbar>
       <Hero></Hero>
+      <Statistics></Statistics>
     </>
   );
 }
