9eb801241fad3f43aedadb33e76e4b2e8eec6d8b
blitz-003 <elmanpathan@gmail.com>
2026-03-31 22:16:07 +0600

    first commit. html css upto hero section done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Navbar/Hero/Hero";
+
+function App() {
+  return (
+    <>
+      <Navbar></Navbar>
+      <Hero></Hero>
+    </>
+  );
+}
+
+export default App;
